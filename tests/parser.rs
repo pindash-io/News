26bc1cfb5d389c021eb8b2cc3fe05a89b5dbd63d
Fangdun Tsai <cfddream@gmail.com>
2023-02-23 18:15:26 +0800

    refactor: parse html x2

--- tests/parser.rs
+++ tests/parser.rs
@@ -1,15 +1,15 @@
-use std::fs;
+use std::{clone, fs};
 
 use anyhow::Result;
 use atoi::ascii_to_digit;
 use ego_tree;
 use feed_rs;
 use pindash_news::easymark;
-use pulldown_cmark::{self, CodeBlockKind, CowStr, Event, HeadingLevel, LinkType, Tag};
+use pulldown_cmark::{self, CodeBlockKind, CowStr, Event, LinkType, Tag};
 use scraper::{
     self,
     node::{Element, Text},
-    Node,
+    ElementRef, Node, Selector,
 };
 
 // use html5ever::tendril::StrTendril;
@@ -20,23 +20,22 @@ const CRTL: &str = "\n";
 // https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
 #[test]
 fn parse_entity() -> Result<()> {
-    // let file = fs::File::open("tests/fixtures/cloudflare.xml")?;
-    // let feed_rs::model::Feed { entries, .. } = feed_rs::parser::parse(file)?;
+    let file = fs::File::open("tests/fixtures/cloudflare.xml")?;
+    let feed_rs::model::Feed { entries, .. } = feed_rs::parser::parse(file)?;
 
-    // let entity = &entries[0];
+    let entity = &entries[0];
 
-    // let content = entity
-    //     .content
-    //     .as_ref()
-    //     .and_then(|c| c.body.clone())
-    //     .unwrap_or_default();
-    //
+    let content = entity
+        .content
+        .as_ref()
+        .and_then(|c| c.body.clone())
+        .unwrap_or_default();
 
-    let content = include_str!("fixtures/simple.html");
+    // let content = include_str!("fixtures/simple.html");
 
     let html = easymark::parser(content);
 
-    let mut events = Vec::<Event<'_>>::new();
+    let events = &mut Vec::<Event<'_>>::new();
     for node in html.root_element().children() {
         // blocks
         match node.value() {
@@ -44,12 +43,11 @@ fn parse_entity() -> Result<()> {
                 let name = elem.name();
                 match name {
                     "h1" | "h2" | "h3" | "h4" | "h5" | "h6" => {
-                        let n = ascii_to_digit::<usize>(name.bytes().nth(1).unwrap()).unwrap();
-                        let tag =
-                            Tag::Heading(HeadingLevel::try_from(n).unwrap(), None, Vec::new());
+                        let level = ascii_to_digit::<usize>(name.bytes().nth(1).unwrap()).unwrap();
+                        let tag = Tag::Heading(level.try_into().unwrap(), None, Vec::new());
                         events.push(Event::Start(tag.clone()));
 
-                        parse_inline(&mut events, node);
+                        parse_inline(events, node);
 
                         events.push(Event::End(tag));
                     }
@@ -57,33 +55,30 @@ fn parse_entity() -> Result<()> {
                         let tag = Tag::Paragraph;
                         events.push(Event::Start(tag.clone()));
 
-                        parse_inline(&mut events, node);
+                        parse_inline(events, node);
 
                         events.push(Event::End(tag));
-                    }
-                    "br" => {
-                        events.push(Event::HardBreak);
                     }
                     "img" => {
                         let mut attrs = elem
                             .attrs()
                             .into_iter()
                             .filter(|a| a.0 == "src" || a.0 == "alt")
-                            .map(|a| (a.0.to_string(), a.1.to_string()))
+                            .map(|a| (a.0, a.1))
                             .collect::<Vec<_>>();
 
-                        attrs.sort_by_key(|attr| attr.0.clone());
+                        attrs.sort_by_key(|attr| attr.0);
 
                         if attrs.is_empty() {
                             continue;
                         }
 
                         let (src, alt) = (
-                            attrs[0].1.clone(),
+                            attrs[0].1.to_string(),
                             if attrs.len() == 1 {
                                 String::new()
                             } else {
-                                attrs[1].1.clone()
+                                attrs[1].1.to_string()
                             },
                         );
 
@@ -95,24 +90,65 @@ fn parse_entity() -> Result<()> {
                         let tag = Tag::BlockQuote;
                         events.push(Event::Start(tag.clone()));
 
-                        parse_inline(&mut events, node);
+                        parse_inline(events, node);
 
                         events.push(Event::End(tag));
                     }
                     "ol" | "ul" => {
                         parse_list(
-                            &mut events,
+                            events,
                             node,
                             (name.chars().next() == Some('o')).then_some(0),
                         );
                     }
+                    "br" => {
+                        events.push(Event::HardBreak);
+                    }
                     "hr" => {
                         events.push(Event::Rule);
                     }
-                    // "pre" => {}
-                    // "code" => {
-                    //     event.replace(Event::Code(CowStr::Borrowed("")));
-                    // }
+                    "pre" => {
+                        let mut kind = CodeBlockKind::Indented;
+                        let elem_ref = ElementRef::wrap(node.into()).unwrap();
+                        let mut text = String::new();
+                        elem_ref.text().collect::<Vec<_>>().iter().for_each(|s| {
+                            text.push_str(s);
+                        });
+
+                        if let Some(k) = elem
+                            .classes()
+                            .find_map(|name| name.split_once("language-"))
+                            .map(|(_, lang)| CodeBlockKind::Fenced(CowStr::Borrowed(lang)))
+                        {
+                            // prism
+                            kind = k;
+                        } else if elem.classes().find(|name| *name == "highlight").is_some() {
+                            // highlight
+
+                            let selector = Selector::parse("code").unwrap();
+
+                            if let Some(code) = elem_ref.select(&selector).next() {
+                                if let Some(k) = code.value() 
+                                    .attrs()
+                                    .find(|attr| attr.0 == "data-lang")
+                                    .map(|(_, lang)| CodeBlockKind::Fenced(CowStr::Borrowed(lang)))
+                                {
+                                    kind = k;
+                                }
+                            }
+                        }
+
+                        // TODO: https://shiki.matsu.io/
+
+                        let tag = Tag::CodeBlock(kind);
+                        events.push(Event::Start(tag.clone()));
+
+                        events.push(Event::Text(CowStr::Boxed(text.into())));
+
+                        events.push(Event::End(tag));
+                    }
+                    // "code" => {}
+                    // foot
                     _ => {}
                 }
             }
@@ -136,32 +172,26 @@ fn parse_list<'a>(
     events.push(Event::Start(tag.clone()));
 
     for node in parent.children() {
-        match node.value() {
-            Node::Element(elem) if elem.name() == "li" => {
-                let tag = Tag::Item;
-                events.push(Event::Start(tag.clone()));
+        if matches!(node.value(), Node::Element(elem) if elem.name() == "li") {
+            let tag = Tag::Item;
+            events.push(Event::Start(tag.clone()));
 
-                for sub_node in node.children() {
-                    match sub_node.value() {
-                        // nested list
-                        Node::Element(sub_elem) => {
-                            if matches!(sub_elem.name(), "ol" | "ul") {
-                                parse_list(
-                                    events,
-                                    sub_node,
-                                    (sub_elem.name().chars().next() == Some('o')).then_some(0),
-                                );
-                            }
-                        }
-                        _ => {
-                            parse_inline(events, node);
-                        }
-                    }
+            for sub_node in node.children() {
+                // nested list
+                if let Some(k) = sub_node
+                    .value()
+                    .as_element()
+                    .map(|elem| elem.name())
+                    .filter(|name| *name == "ol" || *name == "ul")
+                    .and_then(|name| name.chars().next())
+                {
+                    parse_list(events, sub_node, (k == 'o').then_some(0));
+                } else {
+                    parse_inline(events, node);
                 }
-
-                events.push(Event::End(tag));
             }
-            _ => {}
+
+            events.push(Event::End(tag));
         }
     }
 
@@ -172,15 +202,14 @@ fn parse_inline<'a>(events: &mut Vec<Event<'a>>, parent: ego_tree::NodeRef<'a, N
     for node in parent.children() {
         match node.value() {
             Node::Element(elem) => {
-                let name = elem.name();
-                match name {
+                let (start, end) = match elem.name() {
                     // Link
                     "a" => {
                         let mut attrs = elem
                             .attrs()
                             .into_iter()
                             .filter(|a| a.0 == "href" || a.0 == "title")
-                            .map(|a| (a.0.to_string(), a.1.to_string()))
+                            .map(|a| (a.0, a.1))
                             .collect::<Vec<_>>();
 
                         attrs.sort_by_key(|attr| attr.0.clone());
@@ -190,62 +219,54 @@ fn parse_inline<'a>(events: &mut Vec<Event<'a>>, parent: ego_tree::NodeRef<'a, N
                         }
 
                         let (href, title) = (
-                            attrs[0].1.clone(),
+                            attrs[0].1.to_string(),
                             if attrs.len() == 1 {
                                 String::new()
                             } else {
-                                attrs[1].1.clone()
+                                attrs[1].1.to_string()
                             },
                         );
 
                         let tag = Tag::Link(LinkType::Inline, href.into(), title.into());
-                        events.push(Event::Start(tag.clone()));
-
-                        parse_inline(events, node);
-
-                        events.push(Event::End(tag));
+                        (Some(Event::Start(tag.clone())), Some(Event::End(tag)))
                     }
                     // Blod
                     "strong" => {
                         let tag = Tag::Strong;
-                        events.push(Event::Start(tag.clone()));
-
-                        parse_inline(events, node);
-
-                        events.push(Event::End(tag));
+                        (Some(Event::Start(tag.clone())), Some(Event::End(tag)))
                     }
                     // Italic
                     "em" => {
                         let tag = Tag::Emphasis;
-                        events.push(Event::Start(tag.clone()));
-
-                        parse_inline(events, node);
-
-                        events.push(Event::End(tag));
+                        (Some(Event::Start(tag.clone())), Some(Event::End(tag)))
                     }
                     // Strikethrough
                     "del" => {
                         let tag = Tag::Strikethrough;
-                        events.push(Event::Start(tag.clone()));
-
-                        parse_inline(events, node);
-
-                        events.push(Event::End(tag));
+                        (Some(Event::Start(tag.clone())), Some(Event::End(tag)))
                     }
                     // Inline Code
-                    "code" => {
-                        if let Some(Text { text }) =
-                            node.first_child().and_then(|node| node.value().as_text())
-                        {
-                            let event = Event::Code(CowStr::Borrowed(text));
-                            events.push(event);
-                        }
-                    }
+                    "code" => (
+                        node.first_child()
+                            .and_then(|node| node.value().as_text())
+                            .map(|text| Event::Code(CowStr::Borrowed(text))),
+                        None,
+                    ),
                     // Subscript
                     // "sub" => {},
                     // Superscript
                     // "sup" => {},
-                    _ => {}
+                    _ => (None, None),
+                };
+
+                if let Some(e) = start {
+                    events.push(e);
+                }
+
+                if let Some(e) = end {
+                    parse_inline(events, node);
+
+                    events.push(e);
                 }
             }
             Node::Text(Text { text }) if text.trim_end_matches(' ') != CRTL => {
